ab00f3862b10f4cc5bdad95c660821daff0126e5
پیام رحیمی <102457132+payamrahimi00@users.noreply.github.com>
2025-05-04 16:25:19 +0330

    Create average-by-for.js

---
+++ average-by-for.js
@@ -0,0 +1,6 @@
+var nums = 0;
+for(var i = 1; i <=5; i++) {
+      nums = nums + Number(prompt("insert numbers:"))
+}
+console.log(nums / 5)
+// if i insert console.log(nums / 5) in for get average on moment  
