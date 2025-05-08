53689f04c659327988fc7509dac9c75afb2f8cee
پیام رحیمی <102457132+payamrahimi00@users.noreply.github.com>
2025-05-08 17:10:30 +0330

    Create sum-all-index-in-aary.js

---
+++ sum-all-index-in-aary.js
@@ -0,0 +1,13 @@
+var i = 0; 
+var my__array = [];
+var sum = 0;
+while (i < 3) {
+    var sun =  Number(prompt("Insert your value:"));
+    sun = +sun;
+    my__array.push(sun)
+    console.log(my__array)
+    sum = +sum;
+    sum = sum + my__array[i]
+    i++
+}
+console.log(sum)
