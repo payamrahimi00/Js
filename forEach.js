957ade4a04dd9b8a287853266c42a84bea7ef893
پیام رحیمی <102457132+payamrahimi00@users.noreply.github.com>
2025-05-15 17:05:30 +0330

    Create forEach.js

---
+++ forEach.js
@@ -0,0 +1,16 @@
+var a = ["A", "B", "C", "D"];
+
+a.forEach((b, index) => 
+    console.log(index + " : " + b)
+
+);
+// 
+console.log(" ");
+// 
+var b = ["E", "F", "G", "H"];
+
+b.forEach( function(d, index) {
+    console.log(index + " : " + d)
+
+});
+//function in anonymous mode = just use once 
