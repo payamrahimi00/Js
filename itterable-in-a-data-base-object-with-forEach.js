b64dde3bcd9f51d6b062c18cbf238dc7ed4d9b23
پیام رحیمی <102457132+payamrahimi00@users.noreply.github.com>
2025-05-16 14:33:19 +0330

    Create itterable-in-a-data-base-object-with-forEach.js

---
+++ itterable-in-a-data-base-object-with-forEach.js
@@ -0,0 +1,8 @@
+var default_User = [
+    { name: "Poya", Family: "Rahimi", age: 20, _Email: "Example-one@email.com" },
+    { name: "Portae", Family: "Hardani", age: 19, _Email: "Example-second@email.com" },
+    { name: "Danyal", Family: "Rahmani", age: 31, _Email: "Example-third@email.com" }
+];
+default_User.forEach(function(users){
+    console.log(users)
+})
