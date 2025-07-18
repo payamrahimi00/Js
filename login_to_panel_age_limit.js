e27e8ffd30b34ce6dcacd98f3b05e69ff4b51155
پیام رحیمی <102457132+payamrahimi00@users.noreply.github.com>
2025-07-18 19:55:11 +0330

    Create login_to_panel_age_limit.js

---
+++ login_to_panel_age_limit.js
@@ -0,0 +1,13 @@
+var Age = Number(prompt("Insert First number:"));
+if(Age == !isNaN) {
+    if(Age >= 18 ) {
+        console.log("Welcome to panel")
+    }
+    else {
+        console.log("User has banned.");
+        
+    }
+}
+else {
+    console.log("Error");
+}
