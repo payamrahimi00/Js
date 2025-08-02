ee79a529299f6f098c3ce0618e2802a4c3348c0f
پیام رحیمی <102457132+payamrahimi00@users.noreply.github.com>
2025-08-02 21:00:42 +0330

    Create provinceCityLookup.js

---
+++ provinceCityLookup.js
@@ -0,0 +1,372 @@
+var IranProvinces = [
+    {
+      name: 'Tehran',
+      cities: {
+        'Tehran': true,
+        'Robat Karim': true,
+        'Rey': true,
+        'Varamin': true,
+        'Eslamshahr': true,
+        'Damavand': true,
+        'Shahriar': true,
+        'Qods': true,
+        'Malard': true,
+        'Firuzkuh': true,
+        'Pakdasht': true
+      }
+    },
+    {
+      name: 'Alborz',
+      cities: {
+        'Karaj': true,
+        'Nazarabad': true,
+        'Taleqan': true,
+        'Fardis': true,
+        'Mohammadshahr': true,
+        'Asara': true,
+        'Mahdasht': true
+      }
+    },
+    {
+      name: 'Khuzestan',
+      cities: {
+        'Ahvaz': true,
+        'Abadan': true,
+        'Dezful': true,
+        'Andimeshk': true,
+        'Shush': true,
+        'Khorramshahr': true,
+        'Masjed Soleyman': true,
+        'Izeh': true,
+        'Behbahan': true
+      }
+    },
+    {
+      name: 'Isfahan',
+      cities: {
+        'Isfahan': true,
+        'Kashan': true,
+        'Najafabad': true,
+        'Shahreza': true,
+        'Golpayegan': true,
+        'Natanz': true,
+        'Ardestan': true
+      }
+    },
+    {
+      name: 'Fars',
+      cities: {
+        'Shiraz': true,
+        'Marvdasht': true,
+        'Jahrom': true,
+        'Kazerun': true,
+        'Fasa': true,
+        'Darab': true,
+        'Lar': true
+      }
+    },
+    {
+      name: 'East Azerbaijan',
+      cities: {
+        'Tabriz': true,
+        'Maragheh': true,
+        'Marand': true,
+        'Ahar': true,
+        'Mianeh': true,
+        'Sarab': true
+      }
+    },
+    {
+      name: 'West Azerbaijan',
+      cities: {
+        'Urmia': true,
+        'Khoy': true,
+        'Mahabad': true,
+        'Miandoab': true,
+        'Bukan': true,
+        'Piranshahr': true
+      }
+    },
+    {
+      name: 'Kerman',
+      cities: {
+        'Kerman': true,
+        'Sirjan': true,
+        'Rafsanjan': true,
+        'Bam': true,
+        'Jiroft': true,
+        'Zarand': true
+      }
+    },
+    {
+      name: 'Kermanshah',
+      cities: {
+        'Kermanshah': true,
+        'Islamabad-e Gharb': true,
+        'Paveh': true,
+        'Harsin': true,
+        'Kangavar': true,
+        'Sonnat': true
+      }
+    },
+    {
+      name: 'Gilan',
+      cities: {
+        'Rasht': true,
+        'Bandar-e Anzali': true,
+        'Lahijan': true,
+        'Langarud': true,
+        'Rudsar': true,
+        'Fuman': true
+      }
+    },
+    {
+      name: 'Mazandaran',
+      cities: {
+        'Sari': true,
+        'Babol': true,
+        'Amol': true,
+        'Qaemshahr': true,
+        'Behshahr': true,
+        'Chalus': true
+      }
+    },
+    {
+      name: 'Ardabil',
+      cities: {
+        'Ardabil': true,
+        'Parsabad': true,
+        'Meshginshahr': true,
+        'Germi': true,
+        'Bileh Savar': true,
+        'Namin': true
+      }
+    },
+    {
+      name: 'Hamadan',
+      cities: {
+        'Hamadan': true,
+        'Malayer': true,
+        'Nahavand': true,
+        'Tuyserkan': true,
+        'Kabudarahang': true,
+        'Asadabad': true
+      }
+    },
+    {
+      name: 'Hormozgan',
+      cities: {
+        'Bandar Abbas': true,
+        'Minab': true,
+        'Bandar Lengeh': true,
+        'Qeshm': true,
+        'Kish': true,
+        'Hajiabad': true
+      }
+    },
+    {
+      name: 'Ilam',
+      cities: {
+        'Ilam': true,
+        'Mehran': true,
+        'Dehloran': true,
+        'Darreh Shahr': true,
+        'Abdanan': true,
+        'Eyvan': true
+      }
+    },
+    {
+      name: 'Kurdistan',
+      cities: {
+        'Sanandaj': true,
+        'Saqqez': true,
+        'Marivan': true,
+        'Baneh': true,
+        'Qorveh': true,
+        'Bijar': true
+      }
+    },
+    {
+      name: 'Lorestan',
+      cities: {
+        'Khorramabad': true,
+        'Borujerd': true,
+        'Aligudarz': true,
+        'Dorud': true,
+        'Kuhdasht': true,
+        'Azna': true
+      }
+    },
+    {
+      name: 'Markazi',
+      cities: {
+        'Arak': true,
+        'Saveh': true,
+        'Khomeyn': true,
+        'Delijan': true,
+        'Tafresh': true,
+        'Ashtian': true
+      }
+    },
+    {
+      name: 'Qazvin',
+      cities: {
+        'Qazvin': true,
+        'Takestan': true,
+        'Abyek': true,
+        'Buin Zahra': true,
+        'Alborz': true,
+        'Avaj': true
+      }
+    },
+    {
+      name: 'Qom',
+      cities: {
+        'Qom': true,
+        'Jafarieh': true,
+        'Kahak': true,
+        'Salafchegan': true,
+        'Dastjerd': true,
+        'Qanavat': true
+      }
+    },
+    {
+      name: 'Semnan',
+      cities: {
+        'Semnan': true,
+        'Shahrud': true,
+        'Damghan': true,
+        'Garmsar': true,
+        'Mehdishahr': true,
+        'Aradan': true
+      }
+    },
+    {
+      name: 'Sistan and Baluchestan',
+      cities: {
+        'Zahedan': true,
+        'Zabol': true,
+        'Chabahar': true,
+        'Iranshahr': true,
+        'Saravan': true,
+        'Khash': true
+      }
+    },
+    {
+      name: 'Yazd',
+      cities: {
+        'Yazd': true,
+        'Mehriz': true,
+        'Ardakan': true,
+        'Bafq': true,
+        'Taft': true,
+        'Meybod': true
+      }
+    },
+    {
+      name: 'Zanjan',
+      cities: {
+        'Zanjan': true,
+        'Abhar': true,
+        'Khodabandeh': true,
+        'Mahneshan': true,
+        'Tarom': true,
+        'Soltaniveh': true
+      }
+    },
+    {
+      name: 'Golestan',
+      cities: {
+        'Gorgan': true,
+        'Gonbad-e Kavus': true,
+        'Bandar-e Gaz': true,
+        'Aliabad': true,
+        'Kordkuy': true,
+        'Minudasht': true
+      }
+    },
+    {
+      name: 'North Khorasan',
+      cities: {
+        'Bojnord': true,
+        'Shirvan': true,
+        'Esfarayen': true,
+        'Garmeh': true,
+        'Faruj': true,
+        'Maneh': true
+      }
+    },
+    {
+      name: 'Razavi Khorasan',
+      cities: {
+        'Mashhad': true,
+        'Sabzevar': true,
+        'Neyshabur': true,
+        'Torbat-e Jam': true,
+        'Quchan': true,
+        'Kashmar': true
+      }
+    },
+    {
+      name: 'South Khorasan',
+      cities: {
+        'Birjand': true,
+        'Qaen': true,
+        'Ferdows': true,
+        'Tabas': true,
+        'Boshruyeh': true,
+        'Darmian': true
+      }
+    },
+    {
+      name: 'Bushehr',
+      cities: {
+        'Bushehr': true,
+        'Borazjan': true,
+        'Bandar-e Ganaveh': true,
+        'Dashtestan': true,
+        'Tangestan': true,
+        'Deyr': true
+      }
+    },
+    {
+      name: 'Chaharmahal and Bakhtiari',
+      cities: {
+        'Shahr-e Kord': true,
+        'Borujen': true,
+        'Farsan': true,
+        'Lordegan': true,
+        'Farrokhshahr': true,
+        'Saman': true
+      }
+    },
+    {
+      name: 'Kohgiluyeh and Boyer-Ahmad',
+      cities: {
+        'Yasuj': true,
+        'Dehdasht': true,
+        'Gachsaran': true,
+        'Dogonbadan': true,
+        'Basht': true,
+        'Landeh': true
+      }
+    }
+  ];
+
+  var provinceName = prompt("Insert the name of the province:").toLowerCase();
+
+  // Find the province object that matches the input
+  var province = IranProvinces.find(item => {
+      return item.name.toLowerCase() === provinceName;
+  });
+  
+  if (province) {
+      console.log("Cities in " + "\n" + province.name + ":" +"\n", Object.keys(province.cities));
+      
+  } else {
+      console.log("Province not found!");
+  }
+
+
+
+
